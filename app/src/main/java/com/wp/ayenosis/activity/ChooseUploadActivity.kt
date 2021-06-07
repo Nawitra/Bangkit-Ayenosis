9fee8558e2e954065ec43274ba24a4a5e332c9ef
Glory Puncuna <glorypuncuna@gmail.com>
2021-06-07 19:51:25 +0700

    Sending userData detection to firestore

--- app/src/main/java/com/wp/ayenosis/activity/ChooseUploadActivity.kt
+++ app/src/main/java/com/wp/ayenosis/activity/ChooseUploadActivity.kt
@@ -3,30 +3,36 @@
 package com.wp.ayenosis.activity
 
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.auth.User
 import com.wp.ayenosis.R
-import com.wp.ayenosis.ml.Model1
+import com.wp.ayenosis.ml.ConvertedModel
 import com.wp.ayenosis.model.Detection
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.nio.ByteBuffer
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
 import java.time.LocalDateTime
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.ktx.Firebase
+import com.wp.ayenosis.utils.FirebaseUtils.firebaseAuth
 
 class ChooseUploadActivity : AppCompatActivity() {
     private lateinit var bitmap: Bitmap
+    private lateinit var uid: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,8 +63,8 @@ class ChooseUploadActivity : AppCompatActivity() {
 
         val btnSubmit: Button = findViewById(R.id.btn_submit1)
         btnSubmit.setOnClickListener{
-            var normalP: Float = 0.17F
-            var cataractP: Float = 0.83F
+            val normalP: Float = 0.17F
+            val cataractP: Float = 0.83F
             val dateTime = LocalDateTime.now()
 
             val detection = Detection()
@@ -66,13 +72,36 @@ class ChooseUploadActivity : AppCompatActivity() {
             detection.cataractPercent = cataractP
             detection.timeDate = dateTime
 
+            val data = hashMapOf(
+                "normal" to detection.normalPercent,
+                "cataract" to detection.cataractPercent
+            )
+
+            val db = FirebaseFirestore.getInstance()
+
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                uid = user.uid
+            }
+
+
+
+            db.collection("userData").document(uid).collection("detection")
+                .add(data)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show()
+                }
+
             intentToResult(detection)
 
-        /*
-        val bitmapScaled = Bitmap.createScaledBitmap(bitmap, 192, 256, true)
+/*
+        val bitmapScaled = Bitmap.createScaledBitmap(bitmap, 256, 192, true)
             val byteBuffer = ByteBuffer.allocateDirect(4 * 3 * 192 * 256)
-            for (y in 0 until 256) {
-                for (x in 0 until 192) {
+            for (y in 0 until 192) {
+                for (x in 0 until 256) {
                     val px = bitmapScaled.getPixel(x, y)
 
                     val r = Color.red(px)
@@ -91,21 +120,22 @@ class ChooseUploadActivity : AppCompatActivity() {
 
             val tensorImage = TensorImage(DataType.FLOAT32)
             TensorImage.fromBitmap(bitmapScaled)
-            val model = Model1.newInstance(this)
+            val model = ConvertedModel.newInstance(this)
 
-            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 192, 256, 3),DataType.FLOAT32)
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 192, 256, 3), DataType.FLOAT32)
             inputFeature0.loadBuffer(byteBuffer)
 
             val outputs = model.process(inputFeature0)
             val outputFeature0 = outputs.outputFeature0AsTensorBuffer
             val tvPrediction: TextView = findViewById(R.id.tv_prediction)
-            tvPrediction.text = "tempat taro tes"
+            tvPrediction.text = outputFeature0.floatArray[0].toString()
 
             model.close()
-            */
+*/
         }
 
     }
+
 
     private fun intentToResult(data: Detection) {
         val intent = Intent(this, ShowResultActivity::class.java)
@@ -117,9 +147,9 @@ class ChooseUploadActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         val ivFile: ImageView = findViewById(R.id.iv_file)
         if(requestCode==100 && resultCode == Activity.RESULT_OK && data != null){
-            var filepath: Uri = data.data!!
+            val filepath: Uri = data.data!!
             val bitmap1: Bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filepath)
-            bitmap = Bitmap.createScaledBitmap(bitmap1, 192, 256, true)
+            bitmap = Bitmap.createScaledBitmap(bitmap1, 256, 192, true)
         }else if(requestCode==110 && resultCode == Activity.RESULT_OK){
             bitmap = data?.extras?.get("data") as Bitmap
         }
